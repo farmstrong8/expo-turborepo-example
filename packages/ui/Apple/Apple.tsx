31cde679f4f1e7c0764faa2ab1e5b5d95ab5108c
Ferris Armstrong <fda@bu.edu>
2023-05-31 17:14:23 -0400

    changing apple changes app one

--- packages/ui/Apple/Apple.tsx
+++ packages/ui/Apple/Apple.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 export const Apple = () => {
     return (
-        <View style={{ height: 40, width: 40, backgroundColor: "red" }}>
+        <View style={{ height: 40, width: 40, backgroundColor: "green" }}>
             <Text>Apple</Text>
         </View>
     );
